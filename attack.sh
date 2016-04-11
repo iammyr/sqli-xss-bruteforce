56ea863dc1fe155af249b8a48ffc5635ee344b7b
iammyr <iammyr@email.com>
2016-04-11 16:18:57 +0100

    optimisation

--- attack.sh
+++ attack.sh
@@ -8,6 +8,8 @@
 
 ERROR_ARGS="Error: Argument/s not provided."
 USAGE="Usage: $0 <web app URL> <file listing web app URL suffix to attack> <directory with .pay files containing one malicious payload per line>. Check errors.log for errors."
+# App response indicating that an attack was detected and blocked
+DENIED_MESSAGE="Error processing request."
 ERROR_INVALID="Error: Invalid argument/s provided."
 ERROR_INVALID_CODE=2
 
@@ -44,56 +46,91 @@ init_file(){
    fi
 }
 
+
+#Log eventual errors returned by curl.
 error_check(){
    if [[ $1 == Error* ]]; then
       echo "Error: curl for line $2 in file $3 was not executed." >> errors.log
-      return $FALSE
    fi
-   return $TRUE
 }
 
+
+#Check whether the attack attempt was successfully blocked or not.
+is_success(){
+   if [[ $1 == DENIED_MESSAGE ]]; then
+	return $TRUE
+   fi
+   return $FALSE
+}
+
+
+#Submit an attack by including the malicious payload simply as a POST parameter value.
 attack-simple(){
    echo "Submitting payload $1 from file $2"
    payload="vector=$1"
    status=$(curl -X POST -d "$payload" $3/$4)
-   final=error_check $status $1 $2
-   return $final
+   success=is_success $status
+   if [ ! $success ]; then
+   	error_check $status $1 $2
+   fi
+   return $success
 }
 
+
+#Submit an attack by including the malicious payload as an argument for a call to a stored procedure (within a POST parameter value)
 attack-stored-procedure(){
-               echo "Submitting payload $1 from file $2 (attacking stored procedures)"
-               #escape apostrophes by doubling them (as required by stored procedures to succeed)
-               content=cat $1 | sed 's/'"'"'/'"'"''"'"'/g'
-               payload="vector='verifyUserPassword(foo,$content)'"
-               status=$(curl -X POST -d "$payload" $3/$4)
-               final=error_check $status $1 $2
-               return $final
+   echo "Submitting payload $1 from file $2 (attacking stored procedures)"
+   #escape apostrophes by doubling them (as required by stored procedures to succeed)
+   content=cat $1 | sed 's/'"'"'/'"'"''"'"'/g'
+   payload="vector='verifyUserPassword(foo,$content)'"
+   status=$(curl -X POST -d "$payload" $3/$4)
+   success=is_success $status
+   if [ ! $success ]; then
+        error_check $status $1 $2
+   fi
+   return $success
 }
 
+
+#Submit an attack by including the malicious payload as the value of a request header.
 attack-header-based(){
    echo "Submitting attack vector $1 from file $2 inside the request header"
    payload="vector: $1"
    status=$(curl -X POST -H "$payload" $3/$4)
-   final=error_check $status $1 $2
-   return $final      
+   success=is_success $status
+   if [ ! $success ]; then
+        error_check $status $1 $2
+   fi
+   return $success
 }
 
+#Submit an attack by including the malicious payload as a POST parameter's name.
 attack-param-name(){
    echo "Submitting attack vector $1 from file $2 as the payload parameter's name"
    payload="$1=vector"
    status=$(curl -X POST -H "$payload" $3/$4)
-   final=error_check $status $1 $2
-   return $final
+   success=is_success $status
+   if [ ! $success ]; then
+        error_check $status $1 $2
+   fi
+   return $success
 }
 
+
+#Submit an attack by including the malicious payload as a cookie value.
 attack-cookies(){
    echo "Submitting attack vector $1 from file $2 as cookie"
    payload="vector=$1"
    status=$(curl --cookie "$payload" $3/$4)
-   final=error_check $status $1 $2
-   return $final
+   success=is_success $status
+   if [ ! $success ]; then
+        error_check $status $1 $2
+   fi
+   return $success
 }
 
+
+#Submit a series of attacks for each vulnerable application page found in the list provided.
 attack(){
    path="$3/*"
    echo "path $path"
@@ -107,11 +144,19 @@ attack(){
 		echo "file=$file"
 		echo "1=$1"
 		echo "testcase=$testcase"
-               attack-simple "$line" "$file" "$1" "$testcase"
-	       attack-stored-procedure "$line" "$file" "$1" "$testcase"
-               attack-header-based "$line" "$file" "$1" "$testcase"
-               attack-param-name "$line" "$file" "$1" "$testcase"
-	       attack-cookies "$line" "$file" "$1" "$testcase"
+               success=attack-simple "$line" "$file" "$1" "$testcase"
+	       if [ !$success ]; then
+		       success=attack-stored-procedure "$line" "$file" "$1" "$testcase"
+	       fi
+               if [ !$success ]; then
+	               success=attack-header-based "$line" "$file" "$1" "$testcase"
+               fi
+	       if [ !$success ]; then
+	               attack-param-name "$line" "$file" "$1" "$testcase"
+	       fi
+	       if [ !$success ]; then
+		       attack-cookies "$line" "$file" "$1" "$testcase"
+	       fi
            done < "$file"
          fi
       done
