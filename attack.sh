12f806435eb37d393fba9bc5480e63c2ecedf1a8
iammyr <iammyr@email.com>
2016-04-13 17:31:07 +0100

    cleanup

--- attack.sh
+++ attack.sh
@@ -21,7 +21,6 @@ check_existence(){
      echo $USAGE
      exit $ERROR_INVALID_CODE
    fi
-   return $TRUE
 }
 
 check_validity(){
@@ -31,13 +30,11 @@ check_validity(){
      echo $USAGE
      exit $ERROR_INVALID_CODE
    fi
-   return $TRUE    
 }
 
 check_args(){
    check_existence $1 $2 $3
    check_validity $1 $2 $3
-   return $TRUE
 }
 
 init_file(){
@@ -50,7 +47,7 @@ init_file(){
 #Log eventual errors returned by curl.
 error_check(){
    if [[ $1 == Error* ]]; then
-      echo "Error: curl for line $2 in file $3 was not executed." >> errors.log
+      echo "Error: curl for line $2 in file $3 was not executed. Message: $1" >> errors.log
    fi
 }
 
@@ -67,7 +64,7 @@ is_success(){
 
 #Submit an attack by including the malicious payload simply as a POST parameter value.
 attack_param_value(){
-   #echo "Submitting payload $1 from file $2"
+   echo "Submitting payload $1 from file $2"
    payload="vector=$1"
    status=$(curl -X POST -d "$payload" $3/$4)
    is_success "$status"   
@@ -79,9 +76,9 @@ attack_param_value(){
 
 #Submit an attack by including the malicious payload as an argument for a call to a stored procedure (within a POST parameter value)
 attack_stored_procedure(){
-   #echo "Submitting payload $1 from file $2 (attacking stored procedures)"
+   echo "Submitting payload $1 from file $2 (attacking stored procedures)"
    #escape apostrophes by doubling them (as required by stored procedures to succeed)
-   content=cat $1 | sed 's/'"'"'/'"'"''"'"'/g'
+   content=$(sed 's/'"'"'/'"'"''"'"'/g' <<< "$1")
    payload="vector='verifyUserPassword(foo,$content)'"
    status=$(curl -X POST -d "$payload" $3/$4)
    is_success "$status"
@@ -93,7 +90,7 @@ attack_stored_procedure(){
 
 #Submit an attack by including the malicious payload as the value of a request header.
 attack_header(){
-   #echo "Submitting attack vector $1 from file $2 inside the request header"
+   echo "Submitting attack vector $1 from file $2 inside the request header"
    payload="vector: $1"
    status=$(curl -X POST -H "$payload" $3/$4)
    is_success "$status"
@@ -104,7 +101,7 @@ attack_header(){
 
 #Submit an attack by including the malicious payload as a POST parameter's name.
 attack_param_name(){
-  # echo "Submitting attack vector $1 from file $2 as the payload parameter's name"
+   echo "Submitting attack vector $1 from file $2 as the payload parameter's name"
    payload="$1=vector"
    status=$(curl -X POST -H "$payload" $3/$4)
    is_success "$status"
@@ -116,7 +113,7 @@ attack_param_name(){
 
 #Submit an attack by including the malicious payload as a cookie value.
 attack_cookie(){
- #  echo "Submitting attack vector $1 from file $2 as cookie"
+   echo "Submitting attack vector $1 from file $2 as cookie"
    payload="vector=$1"
    status=$(curl --cookie "$payload" $3/$4)
    is_success "$status"
@@ -129,6 +126,7 @@ attack_cookie(){
 #Submit a series of attacks for each vulnerable application page found in the list provided.
 attack(){
    path="$3/*"
+   attacked=0
    echo "path $path"
    for testcase in $(cat $2); do
       isSuccess=0
@@ -137,38 +135,40 @@ attack(){
             echo "Reading from file $file"
             while IFS='' read -r line || [[ -n "$line" ]] && [ $isSuccess -eq 0 ]; do
                line=$( echo $line | sed s/\"/\\\"/g ) 
-		echo "line=$line"
-		echo "file=$file"
-		echo "1=$1"
-		echo "testcase=$testcase"
-                attack_param_value "$line" "$file" "$1" "$testcase"
+               attack_param_value "$line" $file $1 $testcase
 	       if [ $isSuccess -eq 0 ]; then
-		       attack_stored_procedure "$line" "$file" "$1" "$testcase"
+		       attack_stored_procedure "$line" $file $1 $testcase
 	       fi
                if [ $isSuccess -eq 0 ]; then
-	               attack_header "$line" "$file" "$1" "$testcase"
+	               attack_header "$line" $file $1 $testcase
                fi
 	       if [ $isSuccess -eq 0 ]; then
-	               attack_param_name "$line" "$file" "$1" "$testcase"
+	               attack_param_name "$line" $file $1 $testcase
 	       fi
 	       if [ $isSuccess -eq 0 ]; then
-		       attack_cookie "$line" "$file" "$1" "$testcase"
+		       attack_cookie "$line" $file $1 $testcase
 	       fi
            done < "$file"
          fi
       done
       if [ $isSuccess -eq 1 ]; then
 		echo "Attack against $testcase was successful"
+		attacked=$((attacked+1))
       else
 		echo "Unable to find an effective attack against $testcase"
       fi
    done
 }
 
+#trap times EXIT
+start=$(date +%s.%N)
 echo "Validating arguments..."
 check_args $1 $2 $3
 echo "Arguments successfully validated."
 
 echo "Submitting payloads..."
 attack $1 $2 $3
-
+end=$(date +%s.%N)
+echo "$attacked test cases were successfully attacked."
+duration=$(echo "$end - $start" | bc)
+echo -e "start: $start\nend: $end\nThis task took $duration milliseconds"
